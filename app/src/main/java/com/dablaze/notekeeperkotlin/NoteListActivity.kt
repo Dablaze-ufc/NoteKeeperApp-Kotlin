8b5febc58461c6c6d61c50c380ca691a49049f9b
Dablaze-ufc <chukwuwauchenna@gmail.com>
2020-04-03 19:20:10 +0100

    added menu to move to the next note

--- app/src/main/java/com/dablaze/notekeeperkotlin/NoteListActivity.kt
+++ app/src/main/java/com/dablaze/notekeeperkotlin/NoteListActivity.kt
@@ -1,6 +1,8 @@
 package com.dablaze.notekeeperkotlin
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
@@ -8,6 +10,8 @@ import kotlinx.android.synthetic.main.activity_note_list.*
 
 class NoteListActivity : AppCompatActivity() {
     private lateinit var mNavController: NavController
+    private lateinit var nextMenuItem: MenuItem
+    private lateinit var settingsMenuItem: MenuItem
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,6 +25,36 @@ class NoteListActivity : AppCompatActivity() {
         fab.setOnClickListener { view ->
             mNavController.navigate(R.id.action_FirstFragment_to_SecondFragment)
         }
+        initDestinationListener()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        nextMenuItem = menu?.findItem(R.id.action_next)!!
+        settingsMenuItem = menu.findItem(R.id.action_settings)
+        return true
+    }
+
+    fun initDestinationListener(){
+        mNavController.addOnDestinationChangedListener{controller, destination, arguments ->
+           when(destination.id){
+               R.id.FirstFragment -> hideMenuItems()
+               R.id.SecondFragment -> showMenuItems()
+           }
+
+        }
+    }
+
+    private fun showMenuItems() {
+        nextMenuItem.isVisible = true
+        settingsMenuItem.isVisible = true
+    }
+
+    private fun hideMenuItems() {
+
+        nextMenuItem.isVisible = false
+        settingsMenuItem.isVisible = false
+        
     }
 
 }
